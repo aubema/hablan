0d2c04cc7c23407aaa3bd2a963a65e9f114ce93c
aubema <aubema@localhost>
2019-03-09 16:14:30 -0500

    first version

---
+++ hablan.bash
@@ -0,0 +1,140 @@
+#!/bin/bash 
+#
+#   
+#    Copyright (C) 2019  Martin Aube Mikael Labrecque Vincent 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Contact: martin.aube@cegepsherbrooke.qc.ca
+#
+# =============================
+# 
+# global positioning system
+globalpos () {
+#
+#    reading 10 gps transactions
+#
+     /usr/bin/gpspipe -w -n 10 > /root/coords.tmp &
+     sleep 1
+     killall gpspipe > /dev/null 2>&1
+     /usr/bin/tail -2 /root/coords.tmp | sed 's/,/\n/g' | sed 's/"//g' | sed 's/:/ /g'> /root/bidon.tmp
+     grep lat /root/bidon.tmp > /root/bidon1.tmp
+     read bidon lat bidon1 < /root/bidon1.tmp
+     grep lon /root/bidon.tmp > /root/bidon1.tmp
+     read bidon lon bidon1 < /root/bidon1.tmp
+     grep alt /root/bidon.tmp > /root/bidon1.tmp
+     read bidon alt bidon1 < /root/bidon1.tmp
+     # /bin/echo "GPS is connected, reading lat lon data. Longitude:" $lon
+     if [ -z "${lon}" ]
+     then let lon=0
+          let lat=0
+          let alt=0
+     fi 
+     /bin/echo "GPS gives Latitude:" $lat ", Longitude:" $lon "and Altitude:" $alt
+}
+#
+# ==================================
+# main
+# activate gps option 0=off 1=on
+gpsf=1
+gpsport="ttyACM0"
+nobs=9999  		# number of images to acquire; if 9999 then infinity
+#
+# main loop
+#
+# wait for the gps startup
+/bin/sleep 20
+# reset the gps
+gpsctl -D 5 -x "\xB5\x62\x06\x04\x04\x00\xFF\x87\x00\x00\x94\xF5" /dev/ttyACM0
+# set the gps to airborne < 1g mode
+gpsctl -D 5 -x "\xB5\x62\x06\x24\x24\x00\xFF\x06\x03\x00\x00\x00\x00\x10\x27\x00\x00\x05\x00\xFA\x00\xFA\x00\x64\x00\x2C\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x16\xDC" /dev/ttyACM0
+i=0
+while [ $i -lt $nobs ]
+do time1=`date +%s` # initial time
+   #
+   #  searching for gps
+   #
+   if [ $gpsf -eq 1 ] 
+   then echo "GPS mode activated"
+        if [ `ls /dev | grep $gpsport`  ] 
+        then echo "GPS look present."
+             globalpos
+        else /bin/echo "GPS not present: using coords. from localconfig"
+             #
+             #  reading longitude and latitude from localconfig
+             #
+             if [ `grep -c " " /home/sand/localconfig` -ne 0 ]
+             then /bin/grep Longitude /home/sand/localconfig > /root/ligne.tmp
+                  read bidon lon bidon < /root/ligne.tmp
+                  /bin/grep Latitude /home/sand/localconfig > /root/ligne.tmp
+                  read bidon lat bidon < /root/ligne.tmp
+                  /bin/grep Altitude /home/sand/localconfig > /root/ligne.tmp
+                  read bidon alt bidon < /root/ligne.tmp
+             else 
+                  echo "Please put something in /home/sand/localconfig and restart observe-sqm-stepper.bash."
+             fi
+             # /bin/echo "Latitude:" $lat ", Longitude:" $lon
+        fi
+   else  echo "GPS mode off"
+   fi
+   echo "=========================="
+   echo "Start image acquisition #" $count
+   if [  $nobs != 9999 ] 
+   then let i=i+1 #   never ending loop
+   fi
+   n=0
+   nomfich=`date -u +"%Y-%m-%d"`
+   nomfich=$nomfich".txt"
+   time=`date +%Y-%m-%d" "%H:%M:%S`
+   datetime=`date +%Y-%m-%d-%H-%M-%S`
+   nomfich50=$datetime_50mm.raw  # CHANGER .raw par la bonne extension
+   nomfich8=$datetime_8mm.raw    # CHANGER .raw par la bonne extension
+   y=`date +%Y`
+   mo=`date +%m`
+   d=`date +%d`
+   if [ ! -d /var/www/html/data/$y ]
+   then mkdir /var/www/html/data/$y
+   fi
+   if [ ! -d /var/www/html/data/$y/$mo ]
+   then /bin/mkdir /var/www/html/data/$y/$mo
+   fi
+   # writing into log file
+   echo $time $lat $lon $alt $nomfich50 $nomfich8 >> /var/www/html/data/$y/$mo/$nomfich.log
+   # acquisition de l'image 50mm
+
+   # rename image 50mm
+   mv NOMINCONNU $nomfich50
+
+   # acquisition de l'image 8mm
+
+   # rename image 8mm
+   mv NOMINCONNU $nomfich8
+
+   # backup images
+   cp -f $nomfich50 /var/www/html/data/$y/$mo/$nomfich50
+   mv -f $nomfich50 /mnt/QQPART/$nomfich50
+   cp -f $nomfich8 /var/www/html/data/$y/$mo/$nomfich8
+   mv -f $nomfich8 /mnt/QQPART/$nomfich8
+
+   time2=`date +%s`
+   let idle=20-time2+time1  # one measurement every 20 sec
+   echo $idle $time1 $time2
+   if [ $idle -lt 0 ]
+   then let idle=0
+   fi
+   echo "Wait " $idle "s before next acquisition."
+   /bin/sleep $idle
+done
+echo "End of hablan.bash"
+exit 0
