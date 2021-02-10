7377bdd4b5e5612f38feb18cd70be0de8d2ea6d8
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-09 19:11:31 -0500

    modifs

--- heading_angle.py
+++ heading_angle.py
@@ -72,5 +72,5 @@ if(heading < 0):
 #convert into angle
 heading_angle = int(heading * 180/pi) + declination
 
-print ("Heading Angle = %d°" heading_angle)
+print "Heading Angle = ", heading_angle
 #        sleep(1)
