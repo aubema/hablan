4a367d0024eb1ab813fa21c123f422163b671b75
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-16 15:55:09 -0500

    change service

--- rotate.py
+++ rotate.py
@@ -47,17 +47,17 @@ def setStep(w1, w2, w3, w4):
 # loop through step sequence based on number of steps
 j=0
 if reverse==0:
- for i in range(0, steps):
-  j=j+1
-  if j==1:
-   setStep(1,0,1,0)
-  if j==2:
-   setStep(0,1,1,0)
-  if j==3:
-   setStep(0,1,0,1)
-  if j==4:
-   setStep(1,0,0,1)
-   j=0
+    for i in range(0, steps):
+        j=j+1
+        if j==1:
+            setStep(1,0,1,0)
+        if j==2:
+            setStep(0,1,1,0)
+        if j==3:
+            setStep(0,1,0,1)
+        if j==4:
+            setStep(1,0,0,1)
+            j=0
 
 
 
