cd4cfe0867946fa91191970754ceeb398e9f17df
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-16 15:49:22 -0500

    change service

--- rotate.py
+++ rotate.py
@@ -58,7 +58,8 @@ if reverse==0:
                 #setStep(1,0,0,1)
                 setStep(0,1,0,1)
             if j==4:
-                setStep(1,0,0,1)        
+                setStep(1,0,0,1)
+                j=0
                 #setStep(0,0,0,1)
             #if j==5:
                 #setStep(0,1,0,1)
@@ -68,13 +69,13 @@ if reverse==0:
                 #setStep(0,1,1,0)
             #if j==8:
                 #setStep(0,0,1,0)
-                j=0
+            #     j=0
 
 
 # Reverse previous step sequence to reverse motor direction
 else:
 	for i in range(0, steps):
-    		j=j+1
+            j=j+1
             if j==1:
        			setStep(0,0,1,0)
     		if j==2:
