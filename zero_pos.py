d11ee535e746fc223992321570475fb07dd114f0
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-16 22:13:35 -0500

    pos sensor

--- zero_pos.py
+++ zero_pos.py
@@ -54,7 +54,7 @@ for i in range(0, steps):
         if j==4:
             setStep(1,0,0,1)
             j=0
-    print("titi")
+print("titi")
 # move reverse direction complete turn until sensor activated     		
 if GPIO.input(sensor_gpio)!=0:
     j=0
