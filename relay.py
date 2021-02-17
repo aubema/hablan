0bf6c099d04f88ed638ce6e2304715a14c3923aa
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-17 18:31:38 -0500

    first version

--- relay.py
+++ relay.py
@@ -2,6 +2,7 @@
 # usage relay.py gpio_pin state
 # possible states 1 = on 2 = off
 import RPi.GPIO as GPIO
+import sys
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 RELAY_GPIO = int(sys.argv[1])
 STATE = int(sys.argv[2])
