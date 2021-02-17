c0b28d9ebd12d4d361884d9317aac15f1aba6594
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-16 23:29:22 -0500

    pos sensor

--- read2DHT.py
+++ read2DHT.py
@@ -9,10 +9,16 @@ import adafruit_dht
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
-dhtDevice = adafruit_dht.DHT22(board.D7, use_pulseio=False)
-temperature1_c = dhtDevice.temperature
-humidity1 = dhtDevice.humidity
-dhtDevice = adafruit_dht.DHT22(board.D1, use_pulseio=False)
-temperature2_c = dhtDevice.temperature
-humidity2 = dhtDevice.humidity
-print(temperature1_c, temperature2_c, humidity1, humidity2)
+
+try:
+    dhtDevice = adafruit_dht.DHT22(board.D7, use_pulseio=False)
+    temperature1_c = dhtDevice.temperature
+    humidity1 = dhtDevice.humidity
+    dhtDevice = adafruit_dht.DHT22(board.D1, use_pulseio=False)
+    temperature2_c = dhtDevice.temperature
+    humidity2 = dhtDevice.humidity
+    test=temperature1_c+temperature2_c+humidity1+humidity2
+    print(temperature1_c, temperature2_c, humidity1, humidity2)
+except RuntimeError as error:
+    time.sleep(2.0)
+    continue
