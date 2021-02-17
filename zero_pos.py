91ea2c91371893381b2046ba113fa847b3b8de12
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-16 22:07:56 -0500

    pos sensor

--- zero_pos.py
+++ zero_pos.py
@@ -44,7 +44,7 @@ for i in range(0, steps):
         # stop when encoder found
 	if GPIO.input(sensor_gpio)==0:
 		break
-	j=j+1
+        j=j+1
         if j==1:
             setStep(1,0,1,0)
         if j==2:
@@ -69,6 +69,6 @@ if GPIO.input(sensor_gpio)!=0:
             if j==3:
                 setStep(0,1,1,0)
             if j==4:
-            setStep(1,0,1,0)
+                setStep(1,0,1,0)
             j=0
 setStep(0,0,0,0)
