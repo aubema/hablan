c2355474a42b9ab2d15f674002a0f423f025452d
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-16 22:30:20 -0500

    pos sensor

--- zero_pos.py
+++ zero_pos.py
@@ -45,17 +45,16 @@ for i in range(0, half):
     # stop when encoder found
     if GPIO.input(sensor_gpio)==0:
 		break
-    else:
-        j=j+1
-        if j==1:
-            setStep(1,0,1,0)
-        if j==2:
-            setStep(0,1,1,0)
-        if j==3:
-            setStep(0,1,0,1)
-        if j==4:
-            setStep(1,0,0,1)
-            j=0
+    j=j+1
+    if j==1:
+        setStep(1,0,1,0)
+    if j==2:
+        setStep(0,1,1,0)
+    if j==3:
+        setStep(0,1,0,1)
+    if j==4:
+        setStep(1,0,0,1)
+        j=0
 print("titi")
 # move reverse direction complete turn until sensor activated     		
 if GPIO.input(sensor_gpio)!=0:
@@ -66,16 +65,14 @@ if GPIO.input(sensor_gpio)!=0:
         if GPIO.input(sensor_gpio)==0:
             print("tata")
             break
-        else:
-            print("tutu")
-            j=j+1
-            if j==1:
-                setStep(1,0,0,1)
-            if j==2:
-                setStep(0,1,0,1)
-            if j==3:
-                setStep(0,1,1,0)
-            if j==4:
-                setStep(1,0,1,0)
-                j=0
+        j=j+1
+        if j==1:
+            setStep(1,0,0,1)
+        if j==2:
+            setStep(0,1,0,1)
+        if j==3:
+            setStep(0,1,1,0)
+        if j==4:
+            setStep(1,0,1,0)
+            j=0
 setStep(0,0,0,0)
