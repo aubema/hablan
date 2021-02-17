fbc5c53bfbf19af62e8b03536b9d2634ac14f936
aubema <martin.aube@cegepsherbrooke.qc.ca>
2021-02-17 18:22:40 -0500

    first version

---
+++ relay.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# usage relay.py gpio_pin state
+# possible states 1 = on 2 = off
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+RELAY_GPIO = int(sys.argv[1])
+STATE = int(sys.argv[2])
+if STATE==1:
+    mode="GPIO.HIGH"
+else:
+    mode="GPIO.LOW"
+GPIO.setup(RELAY_GPIO, GPIO.OUT) # GPIO Assign mode
+GPIO.output(RELAIS_1_GPIO, mode) # out
