53d758b9bb6e43521526c7cf1189e727c85ac042
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-11-09 23:27:21 -0500

    rotation transition

--- rotate.py
+++ rotate.py
@@ -46,10 +46,11 @@ def setStep(w1, w2, w3, w4):
   GPIO.output(coil_B_2_pin, w4)
 
 # loop through step sequence based on number of steps
+amp=4
 j=0
 if reverse==0:
     for i in range(0, steps):
-        delay=4*delaym*(1-math.sin(math.pi*i/steps))+delaym
+        delay=amp*delaym*(math.cos(2*math.pi*i/steps))+(2*amp+1)*delaym
         j=j+1
         if j==1:
             setStep(1,0,1,0)
@@ -63,7 +64,7 @@ if reverse==0:
 #    setStep(0,0,0,0)
 else:
     for i in range(0, steps):
-        delay=4*delaym*(1-math.sin(math.pi*i/steps))+delaym    
+        delay=amp*delaym*(math.cos(2*math.pi*i/steps))+(2*amp+1)*delaym  
         j=j+1
         if j==1:
             setStep(1,0,0,1)
