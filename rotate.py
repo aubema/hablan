c07741dcfccfcac87f28de8d7ec0d9c298b8a6d9
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-04-12 15:25:05 -0400

    tuning rotation

--- rotate.py
+++ rotate.py
@@ -18,60 +18,61 @@ if steps<0:
    steps=-1*steps
    reverse=1
 steps=steps-1
-delaym = float(sys.argv[2]) * 0.007*350/steps
+if steps!=0:
+   delaym = float(sys.argv[2]) * 0.007*350/steps
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+   GPIO.setmode(GPIO.BCM)
+   GPIO.setwarnings(False)
 
-# Enable pins for IN1-4 to control step sequence
+   # Enable pins for IN1-4 to control step sequence
 
-coil_A_1_pin = 16
-coil_A_2_pin = 12
-coil_B_1_pin = 20
-coil_B_2_pin = 21
+   coil_A_1_pin = 16
+   coil_A_2_pin = 12
+   coil_B_1_pin = 20
+   coil_B_2_pin = 21
 
-# Set pin states
+   # Set pin states
 
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+   GPIO.setup(coil_A_1_pin, GPIO.OUT)
+   GPIO.setup(coil_A_2_pin, GPIO.OUT)
+   GPIO.setup(coil_B_1_pin, GPIO.OUT)
+   GPIO.setup(coil_B_2_pin, GPIO.OUT)
 
-# Function for step sequence
-def setStep(w1, w2, w3, w4):
-  time.sleep(delay)
-  GPIO.output(coil_A_1_pin, w1)
-  GPIO.output(coil_A_2_pin, w2)
-  GPIO.output(coil_B_1_pin, w3)
-  GPIO.output(coil_B_2_pin, w4)
+   # Function for step sequence
+   def setStep(w1, w2, w3, w4):
+       time.sleep(delay)
+       GPIO.output(coil_A_1_pin, w1)
+       GPIO.output(coil_A_2_pin, w2)
+       GPIO.output(coil_B_1_pin, w3)
+       GPIO.output(coil_B_2_pin, w4)
 
-# loop through step sequence based on number of steps
-j=0
-if reverse==0:
-    for i in range(0, steps):
-        delay=(3*steps/375)*delaym*(1-math.sin(math.pi*i/steps))+delaym
-        j=j+1
-        if j==1:
-            setStep(1,0,1,0)
-        if j==2:
-            setStep(0,1,1,0)
-        if j==3:
-            setStep(0,1,0,1)
-        if j==4:
-            setStep(1,0,0,1)
-            j=0
+   # loop through step sequence based on number of steps
+   j=0
+   if reverse==0:
+       for i in range(0, steps):
+           delay=(3*steps/375)*delaym*(1-math.sin(math.pi*i/steps))+delaym
+           j=j+1
+           if j==1:
+               setStep(1,0,1,0)
+           if j==2:
+               setStep(0,1,1,0)
+           if j==3:
+               setStep(0,1,0,1)
+           if j==4:
+               setStep(1,0,0,1)
+               j=0
 #    setStep(0,0,0,0)
-else:
-    for i in range(0, steps):
-        delay=(3*steps/375)*delaym*(1-math.sin(math.pi*i/steps))+delaym    
-        j=j+1
-        if j==1:
-            setStep(1,0,0,1)
-        if j==2:
-            setStep(0,1,0,1)
-        if j==3:
-            setStep(0,1,1,0)
-        if j==4:
-            setStep(1,0,1,0)
-            j=0
+   else:
+       for i in range(0, steps):
+           delay=(3*steps/375)*delaym*(1-math.sin(math.pi*i/steps))+delaym    
+           j=j+1
+           if j==1:
+               setStep(1,0,0,1)
+           if j==2:
+               setStep(0,1,0,1)
+           if j==3:
+               setStep(0,1,1,0)
+           if j==4:
+               setStep(1,0,1,0)
+               j=0
 #    setStep(0,0,0,0)
