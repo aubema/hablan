ba23a5a7d81bb48b1e0889cafd77a683b452f507
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-04-19 17:37:45 -0400

    test original code

--- calibrate_heading.bash
+++ calibrate_heading.bash
@@ -62,7 +62,7 @@ do let angle=i*360/750
         echo $angle $azim >> /var/www/html/data/$y/$mo/$d/$datetime"_heading_calib.txt"
    fi
    /usr/local/bin/rotate.py $delta 1
-   /bin/sleep 0.75
+   /bin/sleep 2
    let i=i+delta
 done
 /usr/local/bin/rotate.py -750 1
